483fc997b3b8de4423c357cf2a4bfb4f8f204e44
cabourjeily3 <cabourjeily3@gatech.edu>
2026-01-12 11:10:22 -0500

    skills change

--- src/components/SkillsContact.tsx
+++ src/components/SkillsContact.tsx
@@ -6,9 +6,9 @@ import { Mail, Linkedin, Github, Phone, MapPin } from "lucide-react"
 import Link from "next/link"
 
 const skills = {
-  "Hard Skills": ["Python", "PyTorch", "C#", "C++", "ROS", "ROS2", "Gazebo", "RViz", "MATLAB", "LabVIEW", "PSpice", "GitHub", "LaTeX"],
+  "Hard Skills": ["Python", "PyTorch", "C#", "C++", "ROS", "ROS2", "Gazebo", "RViz", "OpenCV", "MATLAB", "LabVIEW", "PSpice", "GitHub", "LaTeX"],
   "Languages": ["English (Fluent)", "French (Fluent)", "Arabic (Fluent)", "Korean (Basic)"],
-  "Soft Skills": ["Time Management", "Communication", "Problem-solving", "Critical Thinking", "Teamwork", "Collaboration"]
+  // "Soft Skills": ["Time Management", "Communication", "Problem-solving", "Critical Thinking", "Teamwork", "Collaboration"]
 }
 
 export function Skills() {
